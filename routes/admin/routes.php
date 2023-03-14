e49bcbf93f0e128e15e51b2c00bd8fdcd879ab91
Joao Pedro Ferraz Soares <joaoferrazp@gmail.com>
2023-03-14 15:46:39 -0300

    refactor: removed unused files

--- routes/admin/routes.php
+++ routes/admin/routes.php
@@ -1,11 +1,7 @@
 <?php
 
-use App\Http\Controllers\BannerController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\OrderController;
-use App\Http\Controllers\OrderManagerController;
 use App\Http\Controllers\ProductController;
-use App\Http\Controllers\ServicesController;
 
 Route::middleware(['auth','adminUser'])->prefix('admin')->group(function () {
 
@@ -24,20 +20,6 @@ Route::middleware(['auth','adminUser'])->prefix('admin')->group(function () {
     Route::get('/produtos/editarProdutos/{id}',[ProductController::class,'editProducts'])->name('admin.products.edit');
     Route::post('/produtos/salvarEdicao',[ProductController::class,'update'])->name('admin.products.update');
 });
-
-Route::post('/meusProdutos/order',[ProductController::class,'changeOrder'])->middleware('auth');
-Route::post('/meusProdutos/availability',[ProductController::class,'changeAvailability'])->middleware('auth');
-Route::delete('/meusProdutos/delete/{id}',[ProductController::class,'destroy'])->middleware('auth');
-Route::post('/pedido/confirmado',[OrderController::class,'saveBudget']);
-Route::get('/pedidos/{id}',[OrderManagerController::class,'manager'])->middleware('auth');
-Route::post('/pedidos/visualizar',[OrderManagerController::class,'showOrder']);
-Route::post('/pedidos/atualizar',[OrderManagerController::class,'updateStatusOrder']);
-Route::post('/novoBanner',[BannerController::class,'create']);
-Route::get('/prestador/registrar', function () {return view('admin.services.servicesRegistration');
-});
-Route::post('/validaCadastroPrestador',[ServicesController::class,'create']);
-Route::get('/prestador/visualizar',[ServicesController::class,'view'])->middleware('auth');
-Route::get('/prestadores',[ServicesController::class,'viewAllServices']);
 
 
 
--- routes/ecommerce/routes.php
+++ routes/ecommerce/routes.php
@@ -3,12 +3,8 @@
 use App\Http\Controllers\HomeController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\OrderController;
-use App\Http\Controllers\OrderManagerController;
 use App\Http\Controllers\ProductController;
 use App\Http\Controllers\ClientController;
-use App\Http\Controllers\DeliveryRouteController;
-use App\Http\Controllers\PaymentController;
-use App\Http\Controllers\PdfController;
 
 
 
@@ -29,14 +25,6 @@ Route::middleware(['auth'])->group(function () {
 
 Route::get('/emContrucao', fn() =>  view('commons.buildingFunction'))->name('building.page');
 
-Route::get('/pedido',[OrderController::class,'newBudget']);
-Route::post('/pesquisaCPF',[ClientController::class,'findClient']);
-Route::get('/formBanner', function () {return view('ecommerce.formBanner') ;});
-Route::get('/confirmarPagamento', function () {return view('ecommerce.paymentProcess');});
-Route::get('/pagamento',[PaymentController::class,'payments']);
-Route::get('/geraPdf',[PdfController::class,'createPdf']);
-Route::post('/frete',[DeliveryRouteController::class,'portage']);
-Route::get('/pesquisaPedido',[OrderManagerController::class,'searchOrder']);
 
 
 
