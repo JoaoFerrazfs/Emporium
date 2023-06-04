d5c162a708e69c5ce4304c18734e1a978c490cb6
Joao Pedro Ferraz Soares <joaoferrazp@gmail.com>
2023-06-04 17:19:56 -0300

    feat: add responsive product page

--- resources/views/ecommerce/components/_productPage.blade.php
+++ resources/views/ecommerce/components/_productPage.blade.php
@@ -1,50 +1,51 @@
-<div class="product-page-container">
-    <div class="product-page-image">
-        <div class="card">
-            <div class="card-content">
-                <img src="{{'/img/products/'. $product->image}}" class="card-img-top img-fluid" alt="singleminded">
-            </div>
-        </div>
-        <div class="price-product-container">
-            <h2 class="unit-product">unidade</h2>
-            <h2 class="price-product"> R$ {{ $product->price }} </h2>
-        </div>
-    </div>
-    <div class="product-page-description ">
-        <div class="card ">
-            <div class="card-content product-page-description-card">
+<div class="container mt-4">
+    <div class="row">
+        <div class="col-md-6">
+            <div class="card">
                 <div class="card-body">
-                    <p class="card-text">
-                        Código do produto: {{ $product->id }}
-                    </p>
-                    <h2 class="card-title">{{$product->name}}</h2>
-                    <p class="card-text">
-                        {{ $product->description }}
-                    </p>
-
-                    <h2 class="card-title">Ingredientes</h2>
-                    <p class="card-text">
-                        {{ $product->ingredientes ?? "alguma coisa" }}
-                    </p>
+                    <img src="{{'/img/products/'. $product->image}}" class="card-img-top img-fluid" alt="singleminded">
                 </div>
             </div>
-        </div>
 
-        <div class="card product-page-botton-container">
-            <div class="card-content">
-                <div class="card-body">
-                    <div class="buttons">
-                        <a id="addToCart" href="{{route('cart')}}" class="btn btn-secondary btn-add-to-cart">Adicionar ao carrinho</a>
-
-                        <a id="buy" href="{{route('freight')}}" class="btn btn-secondary btn-buy">Comprar</a>
+            <div class="card mt-4">
+                <div class="card-body d-flex justify-content-between">
+                    <div>
+                        <h2 class="unit-product">Un</h2>
+                    </div>
+                    <div>
+                        <h2 class="price-product"> R$ {{ $product->price }} </h2>
                     </div>
                 </div>
             </div>
+
+        </div>
+
+        <div class="col-md-6">
+            <div class="card">
+                <div class="card-body">
+                    <p class="card-text"> Código do produto: {{ $product->id }}</p>
+                    <h2 class="card-title">{{$product->name}}</h2>
+                    <p class="card-text">{{ $product->description }}</p>
+                    <h2 class="card-title">Ingredientes</h2>
+                    <p class="card-text">{{ $product->ingredientes ?? "alguma coisa" }}</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="card mt-4">
+            <div class="card-body">
+                <div class="buttons">
+                    <a id="addToCart" href="{{route('cart')}}" class="btn btn-secondary btn-lg btn-add-to-cart">Adicionar ao carrinho</a>
+                    <a id="buy" href="{{route('freight')}}" class="btn btn-secondary btn-lg btn-buy">Comprar</a>
+                </div>
+            </div>
+
+            <div class="d-flex justify-content-start mb-2">
+                <a href="{{route('products.list')}}" class="btn btn-secondary text-truncate text-sm">Voltar</a>
+            </div>
         </div>
 
     </div>
 
-    <div class="buttons">
-        <a href="{{route('products.list')}}" class="btn btn-secondary">Voltar</a>
+
     </div>
-</div>
