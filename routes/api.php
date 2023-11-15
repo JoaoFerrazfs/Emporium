8b998b126480501395aa3372228d53daf58d7eab
JoaoFerrazfs <joaoferrazp@gmail.com>
2023-11-15 14:06:06 -0300

    feat: add group for v1 routes

--- routes/api.php
+++ routes/api.php
@@ -1,13 +1,17 @@
 <?php
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/**
- * Auth routes
- */
-include_once __DIR__.'/apis/auth.php';
+Route::prefix('v1')->group(function () {
 
-/**
- * Products routes
- */
-include_once __DIR__.'/apis/products.php';
+    /**
+     * Auth routes
+     */
+    include_once __DIR__.'/apis/auth.php';
+
+    /**
+     * Products routes
+     */
+    include_once __DIR__.'/apis/products.php';
+});
+
+
