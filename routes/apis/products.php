4c932f943da26f8ee2827ca7d45081512e12a7f0
JoaoFerrazfs <joaoferrazp@gmail.com>
2023-12-16 14:11:25 -0300

    refact: change routes names

--- routes/apis/products.php
+++ routes/apis/products.php
@@ -7,11 +7,10 @@ use Illuminate\Support\Facades\Route;
 Route::prefix('products')->group(function () {
     Route::post('/', [ProductController::class,'store'])->name('createProduct');
 
-
     Route::middleware('api.cache')->group(function () {
 
-        Route::post('/search', [ProductSearchController::class,'search'])->name('productSearch');
-        Route::post('/list', [ProductSearchController::class,'listAvailableProducts'])->name('productSearch');
+        Route::post('/search', [ProductSearchController::class,'search'])->name('product.search');
+        Route::post('/list', [ProductSearchController::class,'listAvailableProducts'])->name('available.products');
     });
 });
 
