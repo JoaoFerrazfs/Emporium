a76bcecb476dd2a0bc8b4fba8f9e50437ba15271
JoaoFerrazfs <joaferrazp@gmail.com>
2024-01-17 14:35:03 -0300

    split bash in a new file

---
+++ .github/workflows/support/handle-branches.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+environmentBranch=$1
+
+echo "Checkout $environmentBranch..."
+git checkout $environmentBranch &> /dev/null
+
+branchFile="./.github/workflows/support/$environmentBranch-branches.txt"
+
+IFS=, read -ra branches < "$branchFile"
+validatedBranches=""
+
+for branch in "${branches[@]}"; do
+    branch=$(echo "$branch" | sed 's/^[ \t]*//;s/[ \t]*$//')
+
+    set +e
+    git ls-remote --exit-code --heads origin "$branch" &> /dev/null
+    branch_exists=$?
+
+    if [[ "$branch_exists" == "0" ]]; then
+      git checkout "$branch" &> /dev/null
+      git log --pretty=format:"%h" master | grep "$(git log --pretty=format:"%h" -1 "$branch")" &> /dev/null
+      branch_merged=$?
+
+      if [[ "$branch_merged" == "0" ]]; then
+        git rebase master
+      fi
+
+      validatedBranches+="$branch,"
+    fi
+done
+
+validatedBranches="${validatedBranches%,}"
+echo "validatedBranches= ${validatedBranches%,}" >> $GITHUB_ENV
