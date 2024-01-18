e9b222b144a2e7997fd983b57c21ab44129f1146
JoaoFerrazfs <joaferrazp@gmail.com>
2024-01-18 14:03:33 -0300

    change validation to one only bash

--- .github/support/handle-environments-branches.sh
+++ .github/support/handle-environments-branches.sh
@@ -15,8 +15,6 @@ if [ ! -e "$branchFile" ]; then
   touch "$branchFile"
 fi
 
-cat "./.github/support/$environmentBranch-branches.txt"
-
 IFS=, read -ra branches < "$branchFile"
 validatedBranches=""
 
@@ -42,4 +40,91 @@ done
 
 validatedBranches="${validatedBranches%,}"
 echo "validatedBranches=${validatedBranches%,}" >> $GITHUB_ENV
-export validatedBranches
+
+
+git checkout master
+echo "Setting up Git user identity..."
+git config --local user.name "$GIT_COMMITTER_NAME"
+git config --local user.email "$GIT_COMMITTER_EMAIL"
+
+echo "Deleting $environmentBranch..."
+git branch -D $environmentBranch &> /dev/null
+
+echo "Recreating $environmentBranch..."
+git checkout -b $environmentBranch
+
+echo  "Branches preexistentes: ${validatedBranches}"
+echo  "Issue existentes: ${validatedIssueBranches}"
+
+if [ -n "${shouldRemove}" ]; then
+    allBranches=$(echo "${validatedBranches}" | tr ',' '\n' | grep -v -wF "${validatedIssueBranches}" | tr '\n' ',' | sed 's/,$//')
+else
+    allBranches="${validatedIssueBranches},${validatedBranches}"
+fi
+
+allBranches=$(echo "$allBranches" | sed 's/ *, */,/g; s/,$//')
+
+echo "Prepared branches: $allBranches "
+
+IFS=, read -ra BranchArray <<< "$allBranches"
+
+error_file="error.log"
+touch "$error_file"
+
+if [ "${allBranches}" ]; then
+
+  for branch in "${BranchArray[@]}"; do
+      echo "Chekout to $branch."
+      git checkout $branch  &> /dev/null
+
+      echo "Rebasing $branch with main..."
+      git rebase master 2>> "$error_file"
+      error=$?
+      if [[ "$error" != "0" ]]; then
+          error=$(tail -n 1 "$error_file")
+          echo "An error occurred while rebasing '$environmentBranch' into '$branch': $error"
+          exit 1
+      else
+          echo -e "Rebase successful\n"
+      fi
+
+      echo "Merge $branch into '$environmentBranch'..."
+
+      git checkout $environmentBranch &> /dev/null
+      git merge $branch --no-edit 2>> "$error_file"
+      error=$?
+      if [[ "$error" != "0" ]]; then
+          error=$(tail -n 1 "$error_file")
+          echo "An error occurred while merge '$branch' into '$environmentBranch': $error"
+          exit 1
+      else
+          echo -e "Merge successful\n"
+      fi
+  done
+
+  echo "Creating new branches file to'$environmentBranch'"
+
+else
+  echo "The new branch '$environmentBranch Branch' will be empty with only master as base"
+fi
+
+supportDir="./.github/support/"
+if [ ! -d "$supportDir" ]; then
+  mkdir -p "$supportDir"
+fi
+branchFile="./.github/support/$environmentBranch-branches.txt"
+echo "$allBranches" > $branchFile
+git add .
+git commit -m "add branches file"
+
+echo "Sending updated '$environmentBranch' to github"
+
+git push --force origin $environmentBranch 2>> "$error_file"
+error=$?
+if [[ "$error" != "0" ]]; then
+ error=$(tail -n 1 "$error_file")
+ echo "An error occurred while merge '$branch' into '$environmentBranch': $error"
+ exit 1
+else
+ echo -e "Merge successful\n"
+fi
